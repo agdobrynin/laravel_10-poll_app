2b034f7efe223c9f17fc1c7311912bae0eb39d99
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-07-02 23:15:01 +0400

    feat: Add key for elements in loops.

--- resources/views/livewire/poll-list.blade.php
+++ resources/views/livewire/poll-list.blade.php
@@ -6,13 +6,13 @@
         </div>
     @endif
     @forelse ($polls as $poll)
-        <div class="mb-4 bg-slate-100 rounded-md px-6 py-2">
+        <div wire:key="poll-{{ $poll->id }}" class="mb-4 bg-slate-100 rounded-md px-6 py-2">
             <h3 class="mb-4 text-xl">
                 Poll: &laquo;{{ $poll->title }}&raquo;
             </h3>
             <div class="text-gray-400 mb-4">click on the option to vote</div>
             @foreach ($poll->options as $option)
-                <div class="mb-2 flex gap-2 justify-between border p-2 bg-white rounded-md hover:shadow-md">
+                <div wire:key="option-{{ $option->id }}" class="mb-2 flex gap-2 justify-between border p-2 bg-white rounded-md hover:shadow-md">
                     <div class="cursor-pointer underline underline-offset-1 decoration-indigo-400 hover:underline-offset-4 hover:decoration-indigo-600" wire:click.prevent="vote({{ $option->id }})">
                         {{ $option->name }}
                     </div>
