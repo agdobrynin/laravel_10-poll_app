c6723e0a5e341fff939fe880fb227fa4ab321325
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-07-03 14:05:05 +0400

    feat: Add loader Create poll form.

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -2,12 +2,17 @@
     <form wire:submit.prevent="createPoll">
         <div class="mb-4">
             <label>Poll title</label>
-            <input type="text" wire:model="title" @class(['border-red-500' => $errors->has('title')])>
+            <input type="text"
+               wire:loading.attr="disabled"
+               wire:model="title"
+                @class(['border-red-500' => $errors->has('title')])>
             @error('title') <span class="error">{{ $message }}</span> @enderror
         </div>
         <div class="flex justify-between">
             <h4 class="text-lg">Poll options</h4>
-            <button class="btn" wire:click.prevent="addOption">Add option</button>
+            <button class="btn"
+                    wire:loading.attr="disabled"
+                    wire:click.prevent="addOption">Add option</button>
         </div>
         @error('options') <span class="error">{{ $message }}</span> @enderror
         <div class="mt-4 mb-4">
@@ -15,6 +20,8 @@
                 <div class="flex items-start gap-4">
                     <div class="grow mb-4">
                         <input type="text"
+                           wire:key="option-{{ $index }}"
+                           wire:loading.attr="disabled"
                            wire:model="options.{{ $index }}"
                             @class(['border-red-500' => $errors->has('options.'.$index)])
                         >
@@ -27,7 +34,12 @@
             @endforeach
         </div>
         <div>
-            <button class="btn w-full" type="submit">Add Poll</button>
+            <button type="submit" class="btn w-full" wire:loading.remove wire:loading.attr="disabled">
+                Add Poll
+            </button>
+            <div class="w-full flex" wire:loading>
+                <div class="w-40 mx-auto"><x-loader message="Sending data" /></div>
+            </div>
         </div>
     </form>
 </div>
